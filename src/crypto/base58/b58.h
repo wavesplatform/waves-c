e81a30ca2ec9297632734fbf5d49851feabef655
Ruslan Osmanov <rrosmanov@gmail.com>
2019-08-19 18:54:58 +0300

    Addd base58_decode_len(unsigned char *out, const char *in, size_t b58sz)

--- src/crypto/base58/b58.h
+++ src/crypto/base58/b58.h
@@ -12,6 +12,7 @@ extern "C" {
 extern bool (*b58_sha256_impl)(void *, const void *, size_t);
 
 extern ssize_t base58_decode(unsigned char *out, const char *in);
+extern ssize_t base58_decode_len(unsigned char *out, const char *in, size_t len);
 extern size_t base58_encode(char* out, const unsigned char* in, size_t in_sz);
 
 extern int b58_length_from_bytes(int byteArrayLength);
